e86e15e2d01272ea2b7f127d2f9be19999ae8a6b
HerrAndersson <anderssonjonas94@gmail.com>
2016-05-03 23:38:02 +0200

    Comparison of all images in the folder Images is now possible

--- Tools/perceptualdiff/run.py
+++ Tools/perceptualdiff/run.py
@@ -1,43 +1,43 @@
 from pdiffer import PDiffer
 import os
 
-pdiffer = PDiffer(bin='perceptualdiff')
-
 def get_num(x):
     return int(''.join(ele for ele in x if ele.isdigit()))
 
-
+pdiffer = PDiffer(bin='perceptualdiff')
 content_list = []
 
+#Get names of all files in the folder
 for content in os.listdir("./Images"): # "." means current directory
     content_list.append(content)
 
-i = 0
-while i < len(content_list):
-    print(content_list[i])
-    print(content_list[i+1])
-    i = i + 2
-
-result = pdiffer.pdiff('Images/log.png', 'Images/dog.png')
-
-str = unicode(result)
-
-#open and erase the contents of the file
+#Open and erase the contents of the output text file
 target = open('OutputTxt/out.txt', 'w')
 target.truncate()
 
-if not result:
-	print('PASS')
-else:
-	print('FAIL')
-	print(get_num(str))
+##Go through and compare all images in the folder. img0 compared to img1, img1 compared to img2 etc.
+i = 0
+while i < len(content_list) - 1:
+    result = pdiffer.pdiff('Images/' + content_list[i], 'Images/' + content_list[i+1], threshold = 0)
 
-if result:
-    target.write(unicode(get_num(str)))
-else:
-    taget.write(unicode(0))
+    str = unicode(result)
+    if not result:
+    	print('PASS')
+    else:
+    	print('FAIL')
+    	print(unicode(get_num(str)) + '\n')
+    
+    if result:
+        target.write(unicode(get_num(str)) + '\n')
+    else:
+        target.write(unicode(0) + '\n')
 
+    i = i + 1
+    
 target.close()
+
+
+
 
 #pdiff('image1.png', 'image2.png',
 #    fov=65,
